d3d37c8c200d1fb55ac3c92a6aa86e861ea4d360
Khoa <ldkhoa.11402@gmail.com>
2023-05-07 16:11:17 +0700

    finish add recipe

--- routes/recipesRoute.js
+++ routes/recipesRoute.js
@@ -1,9 +1,81 @@
 import express, { query, request } from 'express'
-
+import multer from 'multer';
+import recipesService from '../service/recipesService.js';
+import fs from "fs"
 const Router = express.Router();
 
-Router.get('/create', async (req,res,next) => {
-   res.render("vwRecipe/createRecipe")
-})
+const storage = multer.diskStorage({
+   destination: async function (req, file, cb) {
+     cb(null, `./public/images/recipes/${req.RecipeID}`);
+   },
+   filename: function (req, file, cb) {
+      let ID = req.RecipeID;
+      let imageType = file.fieldname
+      let filename = ""
+      if(req.imageCount[imageType] == undefined){
+         req.imageCount[imageType] = 1
+      }else{
+         req.imageCount[imageType] += 1
+      }
+      filename = `${ID}_${imageType}_${req.imageCount[imageType]}`
+     cb(null, filename + ".jpg");
+   },
+ });
 
+ async function createBlankRecipe(poster){
+   let addedRecipeID = await recipesService.addRecipe({poster})
+   return addedRecipeID
+}
+const uploadCreate = multer({
+   storage: storage,
+ });
+ 
+Router.get('/create', async (req,res,next) => {
+   res.render("vwRecipe/createRecipe");
+})
+Router.post('/create',async (req,res,next)=>{
+   let newID = await createBlankRecipe("ldkhoa.11402@gmail.com")
+   const folderName = `./public/images/recipes/${newID}`;
+   fs.mkdir(folderName, (err) => {
+      if (err) {
+        next(err);
+      }
+   });
+   req.RecipeID = newID
+   req.imageCount = {}
+   next()
+},uploadCreate.any(), async (req,res,next) => {
+   let idupdate = req.RecipeID
+   let steps = req.body.step
+   let ingredients = req.body.ingredient
+   delete(req.body.step)
+   delete(req.body.ingredient)
+   let updateData = await recipesService.updateRecipe(idupdate, req.body)
+   for(let i = 1; i < steps.length; i++){
+      let stepDesc = steps[i].name
+      let obj = {
+         recipeID: idupdate,
+         id: i,
+         stepName: stepDesc
+      }
+      let updateStep = await recipesService.addStep(obj)
+   }
+   for(let i = 1; i < ingredients.length; i++){
+      let ingredientName = ingredients[i].name 
+      let ingredientCalories = ingredients[i].calories 
+      let obj = {
+         recipeID: idupdate,
+         id: i,
+         name: ingredientName,
+         calories: ingredientCalories
+      }
+      let updateIngre = await recipesService.addIngredient(obj)
+   }
+   res.render("vwRecipe/createRecipe", {
+      isAlert: true,
+      icon: "success",
+      title: "Your recipe has been shared!",
+    });
+  
+})
 export default Router;
---
+++ service/recipesService.js
@@ -0,0 +1,20 @@
+import database from "../database/db.js"
+
+export default{
+    addRecipe:async(data)=>{
+        const addedRecipe = await database('Recipe').insert(data)
+        return addedRecipe[0];
+    },
+    updateRecipe:async(recipeID, data)=>{
+        const addedRecipe = await database('Recipe').where({id: recipeID}).update(data)
+        return addedRecipe[0];
+    },
+    addIngredient: async(data)=>{
+        const addedIngre = await database('ingredients').insert(data)
+        return addedIngre[0];
+    },
+    addStep: async(data)=>{
+        const addedStep = await database('steps').insert(data)
+        return addedStep[0];
+    }
+}
