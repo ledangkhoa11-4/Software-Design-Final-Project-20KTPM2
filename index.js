b7c6cfc43553f783b235ac8b63ad3b884a3aeb38
Khoa <ldkhoa.11402@gmail.com>
2023-05-08 23:55:30 +0700

    edit recipe (Front End)

--- index.js
+++ index.js
@@ -19,7 +19,13 @@ app.use(
 app.engine('hbs', engine({
     extname: 'hbs',
     helpers:{
-      section: express_handlebars_sections()
+      section: express_handlebars_sections(),
+      addOne(num){
+        return num+1
+      },
+      isGreaterThanOne(num){
+        return num > 1 
+      }
     }
 }));
 app.set('view engine', 'hbs');
