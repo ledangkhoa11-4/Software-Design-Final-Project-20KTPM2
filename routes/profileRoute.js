b517deb73b089b867cb9c21771733250b9645660
phuoc <nguyennhuphuoc2002@gmail.com>
2023-05-10 00:35:00 +0700

    edit account info ui

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -5,6 +5,7 @@ import recipesService from '../service/recipesService.js';
 const Router = express.Router();
 
 
+
  
 Router.get('/sharedrecipe', async (req,res,next) => {
   const list=await recipesService.getAllRecipe()
@@ -14,6 +15,11 @@ Router.get('/sharedrecipe', async (req,res,next) => {
       list,
       isEmpty: list.length===0
    });
+})
+
+Router.get('/account', async (req,res,next) =>{
+   // const infos = await userService.getInfo(res.locals.auth.IDUser || 0);
+   res.render('vwProfile/account');
 })
 
 export default Router;
